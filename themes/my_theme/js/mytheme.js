9ed40a2568a47a5fd0103c79bc7585beda4ebcb0
rahanar <rahanar@gmail.com>
2013-03-22 12:15:40 -0400

    updated repo

---
+++ themes/my_theme/js/mytheme.js
@@ -0,0 +1,29 @@
+Drupal.behaviors.your_module = {
+  attach: function(context) {
+    (function($) {
+      // your code goes here ...
+      $(document).ready(function() {
+       // alert('Hello World.');
+       $('#login .block').prepend('<div class="subtitle">LOG INTO EP</div>');
+       $('#login .primary li:first-child a').prepend('<div class="newtothesite">NEW TO THE SITE?</div>');
+       	$('#login .item-list ul li a').append("<input type='button' class ='facebook' name='questionSub' value='CONNECT WITH FACEBOOK'/>")
+       	.mouseover(function () {
+	        $(this).css();
+    	}); 
+    		$('#login .item-list ul').prepend('<div class="savetime">SAVE TIME WITH ONE CLICK</div>')
+       	.mouseover(function () {
+	        $(this).css();
+    	}); 
+    	$('#login .item-list ul li a img').hide();	 
+                 //$('body').css("background-color:black");
+        })
+        
+        $('#login .item-list').prepend('<div class="subtitle">CONNECT SOCIAL NETWORKS</div>');
+        //Changing the title of social networks
+        //$('#login #content.region h3').prepend('SOCIAL NETWORKS');
+        //setTimeout(function(){$('#login #content.region #divGigyaLogin_p0 tbody tr td table tr td div div div').hide();}, 500);
+
+              
+    })(jQuery)
+  }
+}
