f8621a2d0ccf070c376cc2427d5d91223cbf57e5
MIHO <miho.tomiyama@gmail.com>
2013-03-23 14:07:00 -0400

    theming front page

--- themes/my_theme/js/mytheme.js
+++ themes/my_theme/js/mytheme.js
@@ -4,6 +4,7 @@ Drupal.behaviors.your_module = {
       // your code goes here ...
       $(document).ready(function() {
        // alert('Hello World.');
+        $('#login').hide();
        $('#login .block').prepend('<div class="subtitle">LOG INTO EP</div>');
        $('#login .primary li:first-child a').prepend('<div class="newtothesite">NEW TO THE SITE?</div>');
        	$('#login .item-list ul li a').append("<input type='button' class ='facebook' name='questionSub' value='CONNECT WITH FACEBOOK'/>")
@@ -22,8 +23,14 @@ Drupal.behaviors.your_module = {
         //Changing the title of social networks
         //$('#login #content.region h3').prepend('SOCIAL NETWORKS');
         //setTimeout(function(){$('#login #content.region #divGigyaLogin_p0 tbody tr td table tr td div div div').hide();}, 500);
-
-              
+         $('#login').fadeIn(1200,'swing');
+         //
+         
+      	$('#homepage #branding').append('<div id="description">Event is a service that enables event planners and guests alike to pull feeds from social media such as Facebook, Twitter and Instagram to unify your event experience as it’s happening all in one place. Once it’s all over dont’t forget to come and relive the memories.</div>').hide().fadeIn(800,'swing');
+      	$('#homepage #description').hide().fadeIn(1400,'swing');
+      	$('#homepage #branding').append('<div id="home_main"></div>');
+      	$('#homepage #home_main').hide().fadeIn(1400,'swing');
+      	$('#homepage .menu-310 a').html('<div id="home_button"></div>').hide().fadeIn(1200,'swing');
     })(jQuery)
   }
 }
